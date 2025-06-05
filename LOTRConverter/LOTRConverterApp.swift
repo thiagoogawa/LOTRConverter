44960b58443bc6c5b825f59bf307057518acc066
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-06-05 18:29:00 -0300

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Thiago Ogawa on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
