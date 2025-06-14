f8f45053fd9df12a878544ae322b5d183ffc08a6
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-06-14 06:31:00 -0300

    added SelectCurrency & CurrencyIcon

---
+++ LOTRConverter/CurrencyIcon.swift
@@ -0,0 +1,74 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter
+//
+//  Created by Thiago Ogawa on 07/06/25.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            // Parchment background image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                // Text
+                Text("Select the currency you are starting with:")
+                    .fontWeight(.bold)
+                
+                
+                // Currency icons
+                ZStack(alignment: .bottom) {
+                    // Currency image
+                    Image(.copperpenny)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    // Currency name
+                    Text("Copper Penny")
+                        .padding(3)
+                        .font(.caption)
+                        .frame(maxWidth: .infinity)
+                        .background(.brown.opacity(0.75))
+                }
+                .padding(3)
+                .frame(width: 100, height: 100)
+                .background(.brown)
+                .clipShape(.rect(cornerRadius: 25))
+                
+                // Text
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(.bold)
+                
+                
+                
+                // Currency icons
+                
+                // Done button
+                Button("Done") {
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown.mix(with: .black, by: 0.2))
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+    }
+}
+
+
+#Preview {
+    SelectCurrency()
+}
+
